44bb08b37fba974c126423999eef84dd3533a031
Richard Wasonga <rowwasonga@gmail.com>
2025-06-03 21:06:59 +0300

    added new for loop  examples

--- forloops/for.py
+++ forloops/for.py
@@ -2,3 +2,26 @@ students = ['pascal', 'cristina','christopher','Brian']
 
 for student in students:
     print(student)
+
+for student in students:
+    print(f"Hello, {student.title()}!")
+
+for student in students:
+    print(f"{student} has {len(student)} letters.")
+
+for index, student in enumerate(students):
+    print(f"{index + 1}. {student}")
+
+for student in students:
+    if len(student) > 7:
+        print(f"{student} has a long name.")
+
+formatted_names = []
+
+for student in students:
+    formatted_names.append(student.capitalize())
+
+print(formatted_names)
+
+for student in reversed(students):
+    print(student)
